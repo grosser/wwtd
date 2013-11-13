1a6b1688321f24a847cfc8be238783eecc13a8f2
grosser <grosser.michael@gmail.com>
2013-11-13 14:18:54 -0800

    add rake wwtd:parallel

--- lib/wwtd/tasks.rb
+++ lib/wwtd/tasks.rb
@@ -1,5 +1,12 @@
 require "wwtd"
 
+run_wwtd = lambda { |args| exit 1 unless WWTD.run(args) == 0 }
 task :wwtd do
-  exit 1 unless WWTD.run == 0
+  run_wwtd.call([])
+end
+
+namespace :wwtd do
+  task :parallel do
+    run_wwtd.call(["--parallel"])
+  end
 end
